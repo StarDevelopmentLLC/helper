5c1b0abb28f4b022227751b4095fac04761c1607
Firestar311 <slaterandrew0398@gmail.com>
2024-09-30 12:39:06 -0500

    Moved the reflection module from StarLib to the helper library

---
+++ src/main/java/com/stardevllc/helper/ReflectionHelper.java
@@ -0,0 +1,64 @@
+package com.stardevllc.helper;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public final class ReflectionHelper {
+    
+    public static Method getClassMethod(Class<?> clazz, String name, Class<?>... parameters) {
+        try {
+            return clazz.getDeclaredMethod(name, parameters);
+        } catch (NoSuchMethodException e) {}
+
+        return getClassMethod(clazz.getSuperclass(), name, parameters);
+    }
+    
+    public static Set<Method> getClassMethods(Class<?> clazz) {
+        Set<Method> methods = new LinkedHashSet<>(Arrays.asList(clazz.getDeclaredMethods()));
+        if (clazz.getSuperclass() != null && clazz.getSuperclass() != Object.class) {
+            getClassMethods(clazz.getSuperclass(), methods);
+        }
+        return methods;
+    }
+    
+    public static void getClassMethods(Class<?> clazz, Set<Method> methods) {
+        if (methods == null) {
+            methods = new LinkedHashSet<>();
+        }
+        
+        methods.addAll(Arrays.asList(clazz.getDeclaredMethods()));
+        if (clazz.getSuperclass() != null) {
+            getClassMethods(clazz.getSuperclass(), methods);
+        }
+    }
+    
+    public static Field getClassField(Class<?> clazz, String name) {
+        try {
+            return clazz.getDeclaredField(name);
+        } catch (NoSuchFieldException e) {}
+
+        return getClassField(clazz.getSuperclass(), name);
+    }
+    
+    public static Set<Field> getClassFields(Class<?> clazz) {
+        Set<Field> fields = new LinkedHashSet<>(Arrays.asList(clazz.getDeclaredFields()));
+        if (clazz.getSuperclass() != null) {
+            getClassFields(clazz.getSuperclass(), fields);
+        }
+        return fields;
+    }
+    
+    public static void getClassFields(Class<?> clazz, Set<Field> fields) {
+        if (fields == null) {
+            fields = new LinkedHashSet<>();
+        }
+
+        fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+        if (clazz.getSuperclass() != null) {
+            getClassFields(clazz.getSuperclass(), fields);
+        }
+    }
+}
