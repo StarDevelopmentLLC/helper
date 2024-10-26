3b37d3037fa7c6eb5e9aec42b7548c5ad72666e5
Firestar311 <slaterandrew0398@gmail.com>
2024-10-26 18:14:56 -0500

    Added a map helper class

---
+++ src/main/java/com/stardevllc/helper/MapHelper.java
@@ -0,0 +1,19 @@
+package com.stardevllc.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapHelper {
+    public static <K, V> Map<K, V> of(K firstKey, V firstValue, Object... rawValues) {
+        if (rawValues.length % 2 != 0) {
+            throw new IllegalArgumentException("Number of raw values must be even");
+        }
+
+        Map<K, V> map = new HashMap<>();
+        map.put(firstKey, firstValue);
+        for (int i = 0; i < rawValues.length; i += 2) {
+            map.put((K) rawValues[i], (V) rawValues[i + 1]);
+        }
+        return map;
+    }
+}
