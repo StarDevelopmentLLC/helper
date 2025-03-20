4deaebd04c4e5f60e1d174b92f4c02849f4fb115
Firestar311 <slaterandrew0398@gmail.com>
2025-03-20 10:41:40 -0500

    Added a getValue method to allow retrieving a field value using a dot path syntax

--- src/main/java/com/stardevllc/helper/ReflectionHelper.java
+++ src/main/java/com/stardevllc/helper/ReflectionHelper.java
@@ -7,6 +7,32 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public final class ReflectionHelper {
+    public static Object getValue(Object object, String path) {
+        if (!path.contains(".")) {
+            return getFieldValue(object, path);
+        }
+        
+        String[] split = path.split("\\.");
+        
+        Object newObject = getFieldValue(object, split[0]);
+        String newPath = path.substring(split[0].length() + 1);
+        
+        return getValue(newObject, newPath);
+    }
+    
+    public static Object getFieldValue(Object object, String fieldName) {
+        Field field = getClassField(object.getClass(), fieldName);
+        if (field == null) {
+            return null;
+        }
+        
+        try {
+            field.setAccessible(true);
+            return field.get(object);
+        } catch (Exception e) {
+            return null;
+        }
+    }
     
     public static Method getClassMethod(Class<?> clazz, String name, Class<?>... parameters) {
         try {
